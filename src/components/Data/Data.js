45b5053f92c092d1be4f10752c2b20fa4ce74df8
Estanislao-Berdinas <estanislaoberdinas@gmail.com>
2022-08-08 11:43:19 -0300

    Entrega Item Detail

--- src/components/Data/Data.js
+++ src/components/Data/Data.js
@@ -14,42 +14,84 @@ const data = [{
         "id": 1,
         "nombre": "Huertero",
         "img": "../Image/ofertas1.jpeg",
-        "precio": 5000
+        "precio": 5000,
+        "stock": 6,
+        "descrip": "Excelente para exteriores"
     },
     
     {
         "id": 2,
         "nombre": "Macetero",
         "img": "../Image/ofertas3.jpeg",
-        "precio": 3000
+        "precio": 3000,
+        "stock": 4,
+        "descrip": "Decorá tu espacio"
     },
     
     {
         "id": 3,
         "nombre": "Portafoco",
         "img": "../Image/ofertas5.jpeg",
-        "precio": 2500
+        "precio": 2500,
+        "stock": 8,
+        "descrip":"Renová tu iluminación"
     },
     
     {
         "id": 4,
         "nombre": "Tabla",
         "img": "../Image/categoriahogar2.jpeg",
-        "precio": 2000
+        "precio": 2000,
+        "stock": 10,
+        "descrip": "Ideal para tus asados"
     },
     
     {
         "id": 5,
         "nombre": "Mesa",
         "img": "../Image/novedades1.jpeg",
-        "precio": 8000
+        "precio": 8000,
+        "stock": 2,
+        "descrip":"Aggiorná tu comedor"
     },
     
     {
         "id": 6,
         "nombre": "Espejo",
         "img": "../Image/categoriadeco3.jpeg" ,
-        "precio": 3200
+        "precio": 3200,
+        "stock": 1,
+        "descrip": "Disfruta tu habitación"
+        
+    },
+
+    {
+        "codigo": 7,
+        "nombre": "Mesita Ratona",
+        "img": "./images/categoriahogar1.jpeg" ,
+        "precio": 4000,
+        "stock": 2,
+        "descrip": "Embellecé el living"
+        
+    },
+    
+    {
+        "codigo": 8,
+        "nombre": "Perchero",
+        "img": "./images/categoriadeco1.jpeg" ,
+        "precio": 3800,
+        "stock": 5,
+        "descrip":"Estilo Nórdico"
+        
+    },
+    
+    {
+        "codigo": 9,
+        "nombre": "Baúl",
+        "img": "./images/novedades4.jpeg" ,
+        "precio": 3400,
+        "stock": 3,
+        "descrip": "Optimizá tus lugares"
         
     }
     ]
---
+++ src/components/ItemDetails/ItemDetails.js
@@ -0,0 +1,24 @@
+export default function ItemDetails () {
+    
+    // en el Item, cargar solo nombre e id y hacer que el boton me despligue información complementaria desde el itemdetailscontainer
+
+    //dar un formato distinto a la card de bootstrap
+
+    //itemlistcontainer = al details, promesa, usestate, useffect y dentro traer el componente itemdetails
+
+    // recibe el objeto por prop, de un solo objeto en particular ->imgen titulo descripcion y precio
+    
+    return (
+        <div>
+            <img/>
+            <span>Id:</span>
+            <span>Nombre</span>
+            <span>Apellido</span>
+            <span>Descripcion</span>
+            <span>Precio</span>
+
+        </div>
+    );
+    
+
+}
