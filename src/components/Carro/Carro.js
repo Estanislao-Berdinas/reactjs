e6d70ffbf54ab724e0a7b6395c3eba302bd3ed6d
Estanislao-Berdinas <estanislaoberdinas@gmail.com>
2022-09-09 00:37:24 -0300

    Item Collection 2

--- src/components/Carro/Carro.js
+++ src/components/Carro/Carro.js
@@ -1,3 +1,5 @@
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useCartContext } from "../../Context/CartContext"
 import ItemCarro from "../ItemCarro/ItemCarro";
@@ -6,6 +8,26 @@ import ItemCarro from "../ItemCarro/ItemCarro";
 const Carro = () => {
     const {carro, precioTotal} = useCartContext();
 
+        const orden = {
+            comprador:{
+                nombre: 'Tani Berdiñas',
+                mail: 'tani@gmail.com',
+                telefono: '23456789',
+                direccion: 'tandil'
+                    },
+            items: carro.map(producto=>({id: producto.id, nombre: producto.nombre, precio: producto.precio})),
+            total: precioTotal(),
+
+        }
+        
+        const generaCompra = () => {
+                const DB = getFirestore()
+                const ordenesCollection = collection(DB, 'ordenes');
+                addDoc(ordenesCollection, orden)
+                .then (({id})=> console.log (id))
+                
+        }
+        
     if (carro.length === 0) {
         return(
         <>
@@ -21,6 +43,7 @@ const Carro = () => {
                 carro.map(producto =><ItemCarro key={producto.id} producto={producto}/>)
             }
             <p> Precio total = {precioTotal()}</p>
+            <Button onClick={generaCompra()}> Generar compra </Button>
         </>
         )
     
